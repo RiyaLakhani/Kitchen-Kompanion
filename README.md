57e4ac1846f92c06d0d95fb64bf746df58cdbb4e
Madeline Moldrem <mmoldrem@terpmail.umd.edu>
2026-02-25 22:40:06 -0500

    tab 5 draft

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # Kitchen-Kompanion
 
-Single page app im looking at -mm
 Our tabs r just 6 divs on this one html file
 https://www.geeksforgeeks.org/javascript/create-a-single-page-application-using-html-css-javascript/
 
-On tabs themselves:
+Tabs adapted from
 https://www.w3schools.com/howto/howto_js_tabs.asp
+
+
+Tab 4 html adapted from UMD code.
--- script.js
+++ script.js
@@ -1,22 +1,34 @@
 function openTab(evt, tabName) { // adapted from https://www.w3schools.com/howto/howto_js_tabs.asp
-    // evt = click event
+  // evt = click event
 
-    var i, tabcontent, tablinks; // classes and counter
-  
-    // grab all of the tabs
-    tabcontent = document.getElementsByClassName("tabcontent");
+  var i, tabcontent, tablinks; // classes and counter
 
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none"; // they're all getting hidden
-    }
-  
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-  
-    // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
+  // grab all of the tabs
+  tabcontent = document.getElementsByClassName("tabcontent");
+
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none"; // they're all getting hidden
   }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+function displayForm(evt) {
+  const radioButton = document.querySelector('input[name="selection1"]:checked');
+  const dropdown = document.getElementById("selection2");
+
+  if (!radioButton || !dropdown.value) {
+    document.getElementById("selections").textContent = "Please select both options.";
+  } else {
+    document.getElementById("selections").textContent = `You chose ${radioButton.value} and ${dropdown.value}`;
+  }
+
+}
